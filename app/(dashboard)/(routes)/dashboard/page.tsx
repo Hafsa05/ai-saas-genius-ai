c53668bd491cf3a9ca9626aa33f4a8e5c9de1a20
Hafsa05 <hafsa@g.bracu.ac.bd>
2024-02-29 11:18:50 +0600

    page routing start

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-export default function Home() {
+const DashBoardPage = () => {
   return (
     <div>
       <p className="text-6xl">Hello from Hafsa Mridha</p>
+      <p>(protected)</p>
       <Button variant="destructive" className="text-indigo-400 " size="lg">Hey there </Button>
     </div>
   );
 }
+export default DashBoardPage;
